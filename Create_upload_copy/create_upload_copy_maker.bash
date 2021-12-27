49599ce4cb31fdf14352ee87c1ec2ea3f8bfef36
orelrahum <44848947+orelrahum@users.noreply.github.com>
2021-12-27 17:32:45 +0200

    upload

--- Create_upload_copy/create_upload_copy_maker.bash
+++ Create_upload_copy/create_upload_copy_maker.bash
@@ -1,3 +1,4 @@
+#!/bin/bash
 #############################################################################################################################################################
 #create !
 if [ -d "/images" ]
@@ -17,8 +18,6 @@ echo "All images create"
 #############################################################################################################################################################
 StorageName1= "orelrahumstorage1ubuntu"
 ResourceGroup1="orelrahumubuntu_group"
-storageAcc1=`Get-AzStorageAccount -ResourceGroupName $ResourceGroup1 -Name $StorageName1`
-ctx1 = $storageAcc1.Context
 containerName1 = "quickstartblobs3"
 accountkey1=`az storage account keys list -g $ResourceGroup1 -n $StorageName1 --query "[0].value"`
 SASkey1=`az storage container generate-sas --account-key $accountkey1 --account-name $StorageName1 --expiry 2022-01-01 --name $containerName1 --permissions dlrw`
@@ -35,15 +34,6 @@ sudo azcopy copy "images" $storage1url --recursive=true
 
 ##############################################################################################################################################################
 #get storage2 key
-StorageName2= "orelrahumstorage2ubuntu"
-ResourceGroup2="orelrahumubuntu_group"
-storageAcc2=`Get-AzStorageAccount -ResourceGroupName $ResourceGroup2 -Name $StorageName2`
-ctx2 = $storageAcc2.Context
-containerName2 = "quickstartblobs3"
-accountkey2=`az storage account keys list -g $ResourceGroup2 -n $StorageName2 --query "[0].value"`
-SASkey2=`az storage container generate-sas --account-key $accountkey2 --account-name $StorageName2 --expiry 2022-01-01 --name $containerName2 --permissions dlrw`
-storage1ur2="https://${StorageName2}.blob.core.windows.net/${containerName2}?$SASkey2"
-
 #copy from storage1 to storage2
 
-sudo azcopy copy  $storage1url $storage2url --recursive=true
+#sudo azcopy copy  $storage1url $storage2url --recursive=true
