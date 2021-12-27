3b486006205b2985c24722baa9edd56c7bb14392
orelrahum <44848947+orelrahum@users.noreply.github.com>
2021-12-27 17:38:29 +0200

    push

--- Create_upload_copy/connect_server.bash
+++ Create_upload_copy/connect_server.bash
@@ -2,6 +2,5 @@
 IP=$(az network public-ip show -g orelrahumubuntu_group -n orelrahumserverubuntu-ip --query "ipAddress" | tr -d '\t\r\n')
 IP=${IP:1}
 IP=${IP::-3}
-sshpass -p 'Test12345678' ssh -o "StrictHostKeyChecking no" orelrahum@${IP//[$'\t\r\n ']} 'mkdir 1;'
-sshpass -p 'Test12345678' ssh -o "StrictHostKeyChecking no" orelrahum@${IP//[$'\t\r\n ']} 'mkdir 2;'
-sshpass -p 'Test12345678' ssh -o "StrictHostKeyChecking no" orelrahum@${IP//[$'\t\r\n ']} 'mkdir 3;'
+sshpass -p 'Test12345678' ssh -o "StrictHostKeyChecking no" orelrahum@${IP//[$'\t\r\n ']} 'wget -O create_upload_copy_maker.bash https://dev.azure.com/Orelrahum8/fb5604e4-5a67-475b-909a-cb7bee6ca465/_apis/git/repositories/76f0dc8c-443f-43c0-92c0-b9a34267dee5/items?path=/Create_upload_copy/create_upload_copy_maker.bash&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=main&resolveLfs=true&%24format=octetStream&api-version=5.0&download=true'
+sshpass -p 'Test12345678' ssh -o "StrictHostKeyChecking no" orelrahum@${IP//[$'\t\r\n ']} 'bash create_upload_copy_maker.bash ;'
--- Create_upload_copy/create_upload_copy_maker.bash
+++ Create_upload_copy/create_upload_copy_maker.bash
@@ -1,4 +1,21 @@
 #!/bin/bash
+################################################################################################################################
+#need install on clean ubuntu server
+#Download AzCopy
+#wget https://aka.ms/downloadazcopy-v10-linux
+
+#Expand Archive
+#tar -xvf downloadazcopy-v10-linux
+
+#Move AzCopy to the destination you want to store it
+#sudo cp ./azcopy_linux_amd64_*/azcopy /usr/bin/
+
+#login to azcopy
+#sudo azcopy login --tenant-id "72f988bf-86f1-41af-91ab-2d7cd011db47"
+
+
+
+
 #############################################################################################################################################################
 #create !
 if [ -d "/images" ]
@@ -16,17 +33,17 @@ for num in {1..100}
 echo "All images create"
 
 #############################################################################################################################################################
-StorageName1= "orelrahumstorage1ubuntu"
-ResourceGroup1="orelrahumubuntu_group"
-containerName1 = "quickstartblobs3"
-accountkey1=`az storage account keys list -g $ResourceGroup1 -n $StorageName1 --query "[0].value"`
-SASkey1=`az storage container generate-sas --account-key $accountkey1 --account-name $StorageName1 --expiry 2022-01-01 --name $containerName1 --permissions dlrw`
-storage1url="https://${StorageName1}.blob.core.windows.net/${containerName1}?$SASkey1"
 
-write-host $storage1url
+accountkey1=`az storage account keys list -g orelrahumubuntu_group -n orelrahumstorage1ubuntu --query "[0].value"`
+echo $accountkey1
+SASkey1=`az storage container generate-sas --account-key $accountkey1 --account-name orelrahumstorage1ubuntu --expiry 2022-01-01 --name quickstartblobs3 --permissions dlrw`
+echo $SASkey1
+storage1url="https://orelrahumstorage1ubuntu.blob.core.windows.net/quickstartblobs3?${SASkey1:1:-2}"
+echo $storage1url
+
 
 #upload to storage1
-sudo azcopy copy "images" $storage1url --recursive=true
+sudo azcopy cp "images" $storage1url --recursive=true
 
 
 
@@ -35,5 +52,12 @@ sudo azcopy copy "images" $storage1url --recursive=true
 ##############################################################################################################################################################
 #get storage2 key
 #copy from storage1 to storage2
+accountkey2=`az storage account keys list -g orelrahumubuntu_group -n orelrahumstorage2ubuntu --query "[0].value"`
+echo $accountkey2
+SASkey2=`az storage container generate-sas --account-key $accountkey2 --account-name orelrahumstorage2ubuntu --expiry 2022-01-01 --name quickstartblobs3 --permissions dlrw`
+echo $SASkey2
+storage2url="https://orelrahumstorage2ubuntu.blob.core.windows.net/quickstartblobs3?${SASkey2:1:-2}"
+echo $storage2url
 
-#sudo azcopy copy  $storage1url $storage2url --recursive=true
+
+sudo azcopy cp  $storage1url $storage2url --recursive=true
