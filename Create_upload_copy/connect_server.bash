c7454484355c14050ced7ba193453a9a15c630f1
orelrahum <44848947+orelrahum@users.noreply.github.com>
2021-12-27 13:29:26 +0200

    push

--- Create_upload_copy/connect_server.bash
+++ Create_upload_copy/connect_server.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-IP=$(az network public-ip show -g orelrahum -n orelrahumserverubuntu-ip --query "ipAddress" | tr -d '\t\r\n')
+IP=$(az network public-ip show -g orelrahumubuntu_group -n orelrahumserverubuntu-ip --query "ipAddress" | tr -d '\t\r\n')
 IP=${IP:1}
 IP=${IP::-3}
 sshpass -p 'Test12345678' ssh -o "StrictHostKeyChecking no" orelrahum@${IP//[$'\t\r\n ']} 'mkdir 1;'
