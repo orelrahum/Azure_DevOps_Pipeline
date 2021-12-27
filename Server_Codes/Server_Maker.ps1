7a62cbb7769749062e24225876372dbac8998973
orelrahum <44848947+orelrahum@users.noreply.github.com>
2021-12-27 21:12:44 +0200

    push

--- Server_Codes/Server_Maker.ps1
+++ Server_Codes/Server_Maker.ps1
@@ -1,11 +1,5 @@
 #code PS1 to make new Ubuntu Server
-try 
-{
-    New-AzResourceGroupDeployment -Name orelrahumserverubuntu -ResourceGroupName orelrahumubuntu_group `
-    -TemplateFile Server_Codes\Server_Template.json `
-    -TemplateParameterFile Server_Codes\Server_Parameters.json -ErrorAction Stop
-}
-catch 
-{
-    Write-Host "Server1 Already Exists"
-}
+
+New-AzResourceGroupDeployment -Name orelrahumserverubuntu -ResourceGroupName orelrahumubuntu_group `
+-TemplateFile Server_Codes\Server_Template.json `
+-TemplateParameterFile Server_Codes\Server_Parameters.json
